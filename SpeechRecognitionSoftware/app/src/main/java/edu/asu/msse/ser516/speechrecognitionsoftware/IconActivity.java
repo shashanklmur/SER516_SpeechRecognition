f3521f3262db5677921ed58425e0e811fb8e5a60
shashanklmur <shashanklmur@gmail.com>
2016-04-26 14:43:54 -0700

    increased recognizerIntent time

--- SpeechRecognitionSoftware/app/src/main/java/edu/asu/msse/ser516/speechrecognitionsoftware/IconActivity.java
+++ SpeechRecognitionSoftware/app/src/main/java/edu/asu/msse/ser516/speechrecognitionsoftware/IconActivity.java
@@ -35,6 +35,7 @@ public class IconActivity extends AppCompatActivity {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                     RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 20000000);
             startActivityForResult(intent, REQUEST_CODE);
         } else {
             Toast.makeText(getApplicationContext(), "Please Connect to Internet", Toast.LENGTH_LONG).show();
--- SpeechRecognitionSoftware/app/src/main/java/edu/asu/msse/ser516/speechrecognitionsoftware/MusicActivity.java
+++ SpeechRecognitionSoftware/app/src/main/java/edu/asu/msse/ser516/speechrecognitionsoftware/MusicActivity.java
@@ -28,6 +28,7 @@ public class MusicActivity extends AppCompatActivity {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                     RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 20000000);
             startActivityForResult(intent, REQUEST_CODE);
         }
         else{
