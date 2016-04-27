65043ade444f6300a0e4f22aea3164c1bed23646
shashanklmur <shashanklmur@gmail.com>
2016-04-27 02:12:52 -0700

    added changes to recognizer intent time and to main activity for speech to text intent/view transition

--- SpeechRecognitionSoftware/app/src/main/java/edu/asu/msse/ser516/speechrecognitionsoftware/MainActivity.java
+++ SpeechRecognitionSoftware/app/src/main/java/edu/asu/msse/ser516/speechrecognitionsoftware/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                     RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 20000000);
+            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 10000000);
             startActivityForResult(intent, REQUEST_CODE);
         } else {
             Toast.makeText(getApplicationContext(), "Please Connect to Internet", Toast.LENGTH_LONG).show();
@@ -118,10 +118,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(this, STT.class);
                 startActivity(i);
             }
-			public void onClickSTT(View v) {
-				Intent i = new Intent(this, STT.class);
-				startActivity(i);
-    }
+
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -142,6 +139,10 @@ public class MainActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
 
+    }
+    public void onClickSTT(View v) {
+        Intent i = new Intent(this, STT.class);
+        startActivity(i);
     }
 
     public void onClickPhone(View v) {
